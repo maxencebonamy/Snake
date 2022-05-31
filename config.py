e0e2fe14dfaece848be39736240650f03c43b7c5
Hyunkel <maxencebonamy@gmail.com>
2022-05-31 14:38:08 +0200

    31/05/2022

--- config.py
+++ config.py
@@ -1,15 +1,22 @@
 from pygame.math import Vector2
 
 
+# the dimensions of the window (in pixels)
 WINDOW_SIZE = Vector2(800, 800)
+# the background color of the window
 WINDOW_COLOR = (13, 22, 11)
+# the window refresh rate (in frames per second)
 WINDOW_FPS = 10
 
+# the size of the side of a tile (in pixels)
 TILE_LENGTH = 40
+# the dimensions of a tile (do not modify)
 TILE_SIZE = Vector2(TILE_LENGTH)
+# the two different colors of the tiles
 TILE_COLORS = (
     (173, 214, 68),
     (166, 209, 60)
 )
 
-SNAKE_LENGTH = 20
+# the initial length of the snake
+SNAKE_LENGTH = 1
